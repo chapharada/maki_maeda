a385a6e3a18c94cb1be8264b571a5f97fb3990d0
原田 和樹 <>
2021-09-24 23:38:32 +0900

    ＃

--- src/assets/js/contact/main.js
+++ src/assets/js/contact/main.js
@@ -4,11 +4,37 @@ import axios from 'axios';
 new Vue({
   el: "#wrapper",
   data: {
-    errors: [],
-    form: {
-      name: null,
-      email: null,
-      message: null
+    name: '',
+    email: '',
+    message: '',
+    isError: false
+  },
+  computed: {
+  },
+  methods:{
+    validateForm() {
+      this.submitForm()
+    },
+    setParams() {
+      const params = new URLSearchParams() 
+      params.append('form-name', 'contact')
+      params.append('name', this.name);
+      params.append('email', this.email);
+      params.append('message', this.message);
+      return params
+    },
+    submitForm() {
+      const params = this.setParams()
+      axios.post('/', params)
+      .then(() => {
+        location.href = "/contact/sccess/";
+        sessionStorage.setItem('formcache', true);
+      })
+      .catch(function (error) {
+        console.log( params.toString())
+        console.log(error)
+        console.log('エラーです。')
+      })
     }
   }
 });

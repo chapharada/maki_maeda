bb58047b61e04a9b32e549d0e818a690a01e470d
原田 和樹 <>
2021-12-18 20:35:41 +0900

    root directory_追加

--- dist/_nuxt/static/1639812609/works/8r74f45yyw/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639812609",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002F8r74f45yyw",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
